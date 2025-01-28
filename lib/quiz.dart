0f763057f4acb7cb5f63425c1d59e8596225adfc
Flutter Projects <bmwthriad2023@gmail.com>
2025-01-28 09:29:08 +0600

    second changes

---
+++ lib/quiz.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:second_app/start_screen.dart';
+
+class Quiz extends StatefulWidget {
+  const Quiz({super.key});
+
+  @override
+  State<Quiz> createState() => _QuizState();
+}
+
+class _QuizState extends State<Quiz> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                const Color.fromARGB(255, 62, 11, 109),
+                const Color.fromARGB(255, 97, 4, 97)
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+          child: StartScreen(),
+        ),
+      ),
+    );
+  }
+}
--- lib/start_screen.dart
+++ lib/start_screen.dart
@@ -5,10 +5,38 @@ class StartScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Image.asset('assets/images/quiz-logo.png'),
-      ],
+    return Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Image.asset(
+            'assets/images/quiz-logo.png',
+            width: 300,
+            color: const Color.fromARGB(57, 250, 249, 249),
+          ),
+          SizedBox(
+            height: 80,
+          ),
+          Text(
+            'Learn Flutter the Fun Way!',
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 24,
+            ),
+          ),
+          SizedBox(
+            height: 30,
+          ),
+          OutlinedButton.icon(
+            onPressed: () {},
+            style: OutlinedButton.styleFrom(
+              foregroundColor: Colors.white,
+            ),
+            icon: Icon(Icons.arrow_right_alt_outlined),
+            label: Text('Start Quiz'),
+          )
+        ],
+      ),
     );
   }
 }
