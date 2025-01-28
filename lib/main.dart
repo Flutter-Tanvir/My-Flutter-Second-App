16789db715ebb734343d44fedbda91f6be873ab6
Flutter Projects <bmwthriad2023@gmail.com>
2025-01-28 09:44:55 +0600

    All

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:second_app/start_screen.dart';
+import 'package:second_app/quiz.dart';
 
 void main() {
   runApp(
-    MaterialApp(
-      home: Scaffold(
-        body: Container(
-          decoration: BoxDecoration(
-            gradient: LinearGradient(
-              colors: [
-                const Color.fromARGB(255, 62, 11, 109),
-                const Color.fromARGB(255, 97, 4, 97)
-              ],
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
-            ),
-          ),
-          child: StartScreen(),
-        ),
-      ),
-    ),
+    Quiz(),
   );
 }
